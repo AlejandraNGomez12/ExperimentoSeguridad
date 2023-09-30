728372eaed9f6d83013ee1415435487261c2d0c4
Luz_Ochoa <ls.ochoa@uniandes.edu.co>
2023-09-30 17:29:50 -0500

    guardado inicio de sesion y bloqueo de usuario

--- MicroservicioUsuario/modelos/Usuario.py
+++ MicroservicioUsuario/modelos/Usuario.py
@@ -24,3 +24,25 @@ class UsuarioSchema(SQLAlchemySchema):
 
     id = fields.Integer()
     usuario = fields.String()
+
+class Sesion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(50))
+    fechaIngreso = db.Column(db.DateTime)
+    exitoso = db.Column(db.Boolean)
+
+
+    def dar_atributos(self):
+        return {"id": self.id, "usuario": self.usuario, "fechaIngreso": self.fechaIngreso.value, "exitoso": self.exitoso.value}
+
+
+class SesionSchema(SQLAlchemySchema):
+    class Meta:
+        model = Sesion
+        include_relationships = True
+        load_instance = True
+
+    id = fields.Integer()
+    usuario = fields.String()
+    fechaIngreso = fields.DateTime
+    exitoso = fields.Boolean
--- MicroservicioUsuario/vistas/vistas.py
+++ MicroservicioUsuario/vistas/vistas.py
@@ -1,9 +1,10 @@
 from flask import request
-from ..modelos import db, Usuario, UsuarioSchema
+from ..modelos import db, Usuario, UsuarioSchema, Sesion
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import jwt_required, create_access_token
 import hashlib
+import datetime
 
 usuario_schema = UsuarioSchema()
 
@@ -21,16 +22,33 @@ class VistaUsuario(Resource):
         
 class VistaLogIn(Resource):
     def post(self):
+        usuarioInput = Usuario.query.filter(Usuario.usuario == request.json["usuario"]).first()
         contrasena_encriptada = hashlib.md5(request.json["contrasena"].encode('utf-8')).hexdigest()
+        user_sesion = request.json["usuario"]
         usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
-                                       Usuario.contrasena == contrasena_encriptada).first()        
-        db.session.commit()      
-        if usuario is None:
-            return "El usuario no existe", 404
-        else:
-            token_de_acceso = create_access_token(identity=usuario.id)
-            #guardar session
+                                       Usuario.contrasena == contrasena_encriptada, Usuario.activo == True).first() 
+        db.session.commit()         
+        if usuario is None:  
+            now = datetime.datetime.now()
+            nueva_sesion = Sesion(usuario=user_sesion, fechaIngreso=now, exitoso = False)        
+            db.session.add(nueva_sesion)
+            db.session.commit()
+            numero_de_sesiones_invalidas = Sesion.query.filter(Sesion.exitoso == False).count()
+            mensaje = 'Usuario incorrecto.'
+            if numero_de_sesiones_invalidas > 2:
+                if usuarioInput:                   
+                    usuarioInput.activo = False
+                    db.session.commit()                  
+                mensaje = 'Tiene '+ numero_de_sesiones_invalidas.usuario +' intentos invalidos. Usuario Bloqueado.'
+            return mensaje, 404
+        else:            
+            token_de_acceso = create_access_token(identity=usuario.id) 
+            now = datetime.datetime.now()
+            nueva_sesion = Sesion(usuario=user_sesion, fechaIngreso=now, exitoso = True)        
+            db.session.add(nueva_sesion)
+            db.session.commit()                    
             return {"mensaje": "Inicio de sesión exitoso",
                 "token": token_de_acceso,
-                "id": usuario.id                
+                "id": usuario.id                      
             }
+        
